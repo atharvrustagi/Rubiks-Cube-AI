61cdc22ff1ec3a4b5702a9bb5ef65b485419722f
Atharv Rustagi <atharvrustagi@gmail.com>
2021-02-01 20:43:32 +0530

    Update Input_cube.py

--- Rubik's Cube/Rubik's Cube AI/Input_cube.py
+++ Rubik's Cube/Rubik's Cube AI/Input_cube.py
@@ -18,6 +18,7 @@ alpha, beta = 1e-3, -np.pi/2 + 1e-3
 dalpha, dbeta = 0, 0
 selected_color = 'r'
 state = 1
+solved = False
 
 
 def draw(win):
@@ -39,6 +40,8 @@ def draw(win):
 		if state==1:
 			win.blit(state1_ins, (80, 100))
 		win.blit(use_arrow_keys, (200, 130))
+	else:
+		win.blit(solving_text, (180, 100))
 
 	pg.display.update()
 
@@ -48,7 +51,7 @@ def draw_surface(s, v):
 	for i in range(3):
 		pg.draw.line(win, (32, 32, 32), s[i], s[i+1], 3)
 	pg.draw.line(win, (32, 32, 32), s[0], s[3], 3)
-	# details of each square
+	# index of each square
 	# t = font.render(str(v), True, (0,0,0))
 	# win.blit(t, np.mean(s, axis=0)-np.array([t.get_width()/2, t.get_height()/2]))
 
@@ -113,4 +116,13 @@ while True:
 	keys = pg.key.get_pressed()
 	key_action(keys)
 	draw(win)
+
+	if state>=6 and not solved:
+		try:
+			solution, time = solve_cube(colors)
+			print(solution)
+			solved = True
+		except:
+			print("Invalid cube. Enter again")
+			state = 5
 
