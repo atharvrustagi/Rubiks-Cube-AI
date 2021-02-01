12b5166f7f9adbb322edf9c4a4361929f14b4466
Atharv Rustagi <atharvrustagi@gmail.com>
2021-02-01 14:47:07 +0530

    Manually Inputting the cube

---
+++ Rubik's Cube/Rubik's Cube AI/Input_cube.py
@@ -0,0 +1,127 @@
+import pygame as pg
+import numpy as np
+from cube_utils import *
+# from draw_utils import *
+
+WIN_SIZE = (1000, 700)
+win = pg.display.set_mode(WIN_SIZE)
+pg.display.set_caption("Cube")
+
+cube = create_cube(50)		# cube with side = 50
+colors = init_colors()
+
+# field of view params
+f, Zv = create_params(WIN_SIZE[1], theta=np.pi/2, Zv=1000)
+turn_speed = 100
+key_press = False
+key_count = 0
+alpha, beta = 1e-3, -np.pi/2 + 1e-3
+dalpha, dbeta = 0, 0
+selected_color = 'r'
+state = 1
+
+"""
+cube start -> (225, 255)
+cube mid1  -> (308, 308)
+cube mid2  -> (392, 392)
+cube end   -> (475, 475)
+"""
+
+def draw(win):
+	win.fill(WHITE)
+	pg.draw.line(win, BLACK, (WIN_SIZE[1]+50, 0), (WIN_SIZE[1]+50, WIN_SIZE[0]))
+
+	tmp_cube, z = project_surfaces(np.copy(cube))
+	dc = {z[x] : x for x in range(54)}
+	z.sort()
+
+	for k in reversed(z):
+		v = dc[k]
+		draw_surface(tmp_cube[v], v)
+	draw_color_buttons(win, selected_color)
+	draw_prev_next(win)
+	if state==1:
+		win.blit(state1_ins, (80, 100))
+
+	win.blit(use_arrow_keys, (200, 130))
+
+	pg.display.update()
+
+def draw_surface(s, v):
+	# s -> 4, 2-D coordinates in cyclic order
+	pg.draw.polygon(win, colors[v], s)
+	for i in range(3):
+		pg.draw.line(win, (32, 32, 32), s[i], s[i+1], 3)
+	pg.draw.line(win, (32, 32, 32), s[0], s[3], 3)
+	# details of each square
+	# if v==13:
+	# 	t = font.render("Front", True, (0,0,0))
+	# 	win.blit(t, np.mean(s, axis=0)-np.array([t.get_width()/2, t.get_height()/2]))
+	# if v==22:
+	# 	t = font.render("Up", True, (0,0,0))
+	# 	win.blit(t, np.mean(s, axis=0)-np.array([t.get_width()/2, t.get_height()/2]))
+	t = font.render(str(v), True, (0,0,0))
+	win.blit(t, np.mean(s, axis=0)-np.array([t.get_width()/2, t.get_height()/2]))
+
+def project_surfaces(cube):
+	h = (cube[..., 0]**2 + cube[..., 2]**2)**0.5
+	a = np.arctan(cube[..., 2]/(cube[..., 0] + 1e-8)) - (alpha+dalpha)
+	c = np.where(cube[..., 0]>=0, 1, -1)
+	cube[..., 0] = c*h*np.cos(a)
+	cube[..., 2] = c*h*np.sin(a)
+
+	h = (cube[..., 1]**2 + cube[..., 2]**2)**0.5
+	a = np.arctan(cube[..., 2]/(cube[..., 1] + 1e-8)) - (beta+dbeta)
+	c = np.where(cube[..., 1]>=0, 1, -1)
+	cube[..., 1] = c*h*np.cos(a)
+	cube[..., 2] = c*h*np.sin(a)
+
+	z = np.mean(cube[..., 2], axis=3).reshape(54)
+	return WIN_SIZE[1]//2 + (f*cube[..., :2]/(Zv+cube[..., 2:])).reshape(54, 4, 2), z
+
+def key_action(keys):
+	global alpha, beta, dalpha, dbeta
+	if keys[pg.K_UP] and dbeta <= np.pi/2:
+		dbeta += np.pi/2/turn_speed
+	elif keys[pg.K_DOWN] and dbeta >= -np.pi/2:
+		dbeta -= np.pi/2/turn_speed
+	elif keys[pg.K_LEFT] and dalpha <= np.pi/2:
+		dalpha += np.pi/2/turn_speed
+	elif keys[pg.K_RIGHT] and dalpha >= -np.pi/2:
+		dalpha -= np.pi/2/turn_speed
+	else:
+		if dalpha>1e-3:
+			dalpha -= np.pi/2/turn_speed
+		elif dalpha<-1e-3:
+			dalpha += np.pi/2/turn_speed
+
+		if dbeta>1e-3:
+			dbeta -= np.pi/2/turn_speed
+		elif dbeta<-1e-3:
+			dbeta += np.pi/2/turn_speed
+
+def state_change(os, ns):
+	global alpha, beta
+	factors = changes(os, ns)
+	for _ in range(turn_speed):
+		alpha += factors[0]*np.pi/turn_speed/2
+		beta += factors[1]*np.pi/turn_speed/2
+		draw(win)
+
+
+while True:
+	for event in pg.event.get():
+		if event.type==pg.QUIT:
+			pg.quit()
+			exit()
+		elif event.type==pg.MOUSEBUTTONDOWN:
+			pos = pg.mouse.get_pos()
+			selected_color, new_state = mouse_action(pos, selected_color, state)
+			if state != new_state:
+				state_change(state, new_state)
+				state = new_state
+
+	keys = pg.key.get_pressed()
+	key_action(keys)
+	draw(win)
+
---
+++ Rubik's Cube/Rubik's Cube AI/cube_utils.py
@@ -0,0 +1,145 @@
+import pygame as pg
+import numpy as np
+
+# clrs = {0:(255, 0, 38), 1:(36, 255, 50), 2:(255, 238, 0), 3:(255, 100, 0), 4:(21, 113, 243), 5:(255, 255, 255)}
+RED = (255, 0, 38)
+GREEN = (36, 255, 50)
+YELLOW = (255, 238, 0)
+ORANGE = (255, 100, 0)
+BLUE = (21, 113, 243)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+"""
+changes:
+yellow - green - orange - blue - red - white
+"""
+
+
+def changes(os, ns):
+	if os==1 and ns==2:
+		return (0, 1)
+	if os==2 and ns==1:
+		return (0, -1)
+	if os==2 and ns==3:
+		return (1, 0)
+	if os==3 and ns==2:
+		return (-1, 0)
+	if os==3 and ns==4:
+		return (1, 0)
+	if os==4 and ns==3:
+		return (-1, 0)
+	if os==4 and ns==5:
+		return (1, 0)
+	if os==5 and ns==4:
+		return (-1, 0)
+	if os==5 and ns==6:
+		return (0, 1)
+	if os==6 and ns==5:
+		return (0, -1)
+
+def create_cube(side = 50):
+	s = side
+	surfaces = np.zeros((6, 3, 3, 4, 3))	# 6 faces, 3x3 squares each, 4 (x,y,z) coordinates for each squares
+	# left face, centre
+	surfaces[0, 1, 1] = np.array([[-3*s, -s, s], [-3*s, -s, -s], [-3*s, s, -s], [-3*s, s, s]])
+	# front face, centre
+	surfaces[1, 1, 1] = np.array([[-s, s, -3*s], [-s, -s, -3*s], [s, -s, -3*s], [s, s, -3*s]])
+	# top face, centre
+	surfaces[2, 1, 1] = np.array([[-s, -3*s, -s], [-s, -3*s, s], [s, -3*s, s], [s, -3*s, -s]])
+
+	for i in range(3):
+		for j in range(3):
+			# left face
+			surfaces[0, i, j] = surfaces[0, 1, 1]
+			surfaces[0, i, j, :, 2] -= (i-1)*2*s
+			surfaces[0, i, j, :, 1] += (j-1)*2*s
+
+			# front face
+			surfaces[1, i, j] = surfaces[1, 1, 1]
+			surfaces[1, i, j, :, 0] += (i-1)*2*s
+			surfaces[1, i, j, :, 1] += (j-1)*2*s
+
+			# top face
+			surfaces[2, i, j] = surfaces[2, 1, 1]
+			surfaces[2, i, j, :, 0] += (i-1)*2*s
+			surfaces[2, i, j, :, 2] -= (j-1)*2*s
+
+	# right face
+	surfaces[3] = surfaces[0]
+	surfaces[3, ..., 0] += 6*s
+	surfaces[3, ..., 2] *= -1
+
+	# back face
+	surfaces[4] = surfaces[1]
+	surfaces[4, ..., 2] += 6*s
+	surfaces[4, ..., 0] *= -1
+
+	# bottom face
+	surfaces[5] = surfaces[2]
+	surfaces[5, ..., 1] += 6*s
+	surfaces[5, ..., 0] *= -1
+
+	return surfaces
+
+def create_params(W, theta, Zv):
+	return W/np.tan(theta/2), Zv
+
+
+clrs = {'r':(255, 0, 38), 'g':(36, 255, 50), 'y':(255, 238, 0), 
+		'o':(255, 100, 0), 'b':(21, 113, 243), 'w':(255, 255, 255)}
+
+def init_colors():
+	colors = np.zeros((54, 3))
+	for i, k in enumerate(clrs.keys()):
+		colors[i*9:i*9+9] = clrs[k]
+	return colors
+
+color_buttons = {'r':np.array([850, 300]), 'g':np.array([850, 350]), 'b':np.array([850, 400]),
+				 'o':np.array([850, 450]), 'y':np.array([850, 500]), 'w':np.array([850, 550])}
+
+button_size = 50
+
+pg.font.init()
+font = pg.font.SysFont("georgia", 20)
+
+state1_ins = font.render("Hold the Cube with Yellow face in front and Blue face on top", True, BLACK)
+use_arrow_keys = font.render("Use Arrow keys to scout the cube", True, BLACK)
+np_width = 150
+np_height = 75
+prev_text = font.render("Previous", True, WHITE)
+next_text = font.render("Next", True, WHITE)
+np_coors = np.array([[100, 600], [500, 600]])
+
+def draw_prev_next(win):
+	pg.draw.rect(win, BLACK, (np_coors[0, 0], np_coors[0, 1], np_width, np_height))
+	pg.draw.rect(win, BLACK, (np_coors[1, 0], np_coors[1, 1], np_width, np_height))
+	win.blit(prev_text, (np_coors[0, 0]+np_width//2-prev_text.get_width()//2,
+			 np_coors[0, 1]+np_height//2-prev_text.get_height()//2))
+	win.blit(next_text, (np_coors[1, 0]+np_width//2-next_text.get_width()//2,
+			 np_coors[1, 1]+np_height//2-next_text.get_height()//2))
+
+def draw_color_buttons(win, s):
+	for k in color_buttons.keys():
+		pg.draw.rect(win, clrs[k], (color_buttons[k][0], 
+			color_buttons[k][1], button_size, button_size))
+	pg.draw.rect(win, (75, 75, 75), (847, 297, button_size+5, 6*button_size+5), 5)
+	pg.draw.rect(win, BLACK, (color_buttons[s][0]-3, 
+			color_buttons[s][1]-3, button_size+5, button_size+5), 5)
+
+
+def mouse_action(pos, scolor, state):
+	if np_coors[0, 1] <= pos[1] <= np_coors[0, 1] + np_height:
+		if np_coors[0, 0] <= pos[0] <= np_coors[0, 0] + np_width:
+			print("Previous")
+			return scolor, (state-1) if state>1 else 1
+		elif np_coors[1, 0] <= pos[0] <= np_coors[1, 0] + np_width:
+			print("Next")
+			return scolor, (state+1) if state<6 else 6
+
+	if 850 <= pos[0] <= 900:
+		for k in color_buttons.keys():
+			if color_buttons[k][1] <= pos[1] <= color_buttons[k][1] + 50:
+				return k, state
+	return scolor, state
+
